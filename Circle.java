7a43ece5487f3e43f23fa88b6bcfe85cb6a9a192
anikait kadamb <anikaitkadamb@anikaits-MacBook-Pro.local>
2024-10-26 19:53:43 -0400

    Add Circle.java

---
+++ Circle.java
@@ -0,0 +1,18 @@
+public class Circle {
+    Point C;
+    double rad;
+
+    public Circle(Point C, double rad){
+        this.C = C;
+        this.rad = rad;
+    }
+
+    public double perimeter(){
+        return 2*Math.pi*this.rad;
+    }
+
+    public boolean isPointInside(Point x){
+        double dist = x.distance(this.C);
+        if(dist <= rad) return true;
+        return false;
+    }
