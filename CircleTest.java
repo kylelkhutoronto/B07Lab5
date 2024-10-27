0929fc451fb823ccb17e3a1a83596885d3122872
Harshita Duseja <harshitaduseja@Harshitas-MacBook-Air.local>
2024-10-27 12:43:54 -0400

    Add CircleTest.java

---
+++ CircleTest.java
@@ -0,0 +1,16 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class CircleTest{
+
+    @Test
+    void testPerimeter(){
+        Point center = new Point(0.0, 0.0);
+        Circle cl = new Circle(center, 1.0);
+
+        assertEquals(2* Math.PI * 1.0, c1.perimeter(), 0.0001);
+        Circle c2 = new Circle(center, 2.5);
+        assertEquals(2* Math.PI * 2.5, c2.perimeter(), 0.0001);
+    }
+
+}
